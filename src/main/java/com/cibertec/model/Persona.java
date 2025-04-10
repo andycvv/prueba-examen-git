136d58d342536dc9c40e3ad361f7900e5b9b4cf9
Brahayan05 <brahayanjw@gmail.com>
2025-04-10 13:04:30 -0500

    agregar constructor get y set

--- src/main/java/com/cibertec/model/Persona.java
+++ src/main/java/com/cibertec/model/Persona.java
@@ -1,5 +1,31 @@
 package com.cibertec.model;
 
+import java.util.Objects;
+
 public class Persona {
 	private int id;
+
+	public Persona(int id) {
+		super();
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return id == other.id;
+	}
+	
+	
 }
